c3cefcec48ad21ba11bf5cce14e8663591770b60
poke53280 <anders.topper@gmail.com>
2018-04-12 09:57:29 +0200

    First synthpop interaction

--- main.R
+++ main.R
@@ -1,1 +1,12 @@
-a <- 1
+
+library(synthpop)
+
+### selection of variables
+vars <- c("sex","age","marital","income","ls","smoke")
+ods  <- SD2011[1:1000,vars]
+
+### default synthesis
+s1 <- syn(ods)
+s1
+
+
